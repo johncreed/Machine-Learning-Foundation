a780c7fb71ae4f7e895504e9a92222129c95f7d9
Hsia Jui-Yang <b02701216@ntu.edu.tw>
2016-08-24 02:32:08 +0800

    hw4 16-20

--- Hw4/4_16-18.py
+++ Hw4/4_16-18.py
@@ -11,10 +11,18 @@ with open('./hw4_train.dat','r') as f:
 				tmp = [float(i) for i in tmp]
 				x.append(np.concatenate(([1],tmp[0:len(tmp)-1])).tolist())
 				y.append(tmp[len(tmp)-1])
-
 x_m = np.matrix(x)
 y_v = np.matrix(y).T
-i = np.identity(x_m.shape[1])
+
+x_train_m = np.matrix(x[0:120])
+y_train_v = np.matrix(y[0:120]).T
+i = np.identity(x_train_m.shape[1])
+
+x_val = x[120:200]
+y_val = y[120:200]
+x_val_m = np.matrix(x_val)
+y_val_v = np.matrix(y_val).T
+
 x_out =[]
 y_out = []
 with open('./hw4_test.dat','r') as f:
@@ -23,7 +31,6 @@ with open('./hw4_test.dat','r') as f:
 				tmp = [float(i) for i in tmp]
 				x_out.append(np.concatenate(([1],tmp[0:len(tmp)-1])).tolist())
 				y_out.append(tmp[len(tmp)-1])
-
 x_out_m = np.matrix(x_out)
 y_out_v = np.matrix(y_out).T
 
@@ -41,22 +48,42 @@ def err_fnt(w,x_m,y_v):
 		return (1/float(x_m.shape[0]))*count
 
 lmb_arr = [10**i for i in range(-10,3)]
-e_in_arr = []
+'''
+lmb_arr = []
+for i in range(10000):
+	lmb_arr.append(0.0001*i)
+'''
+e_train_arr = []
+e_val_arr = []
 e_out_arr = []
+
+count = 0
 for lmb in lmb_arr:
+		count = count + 1
+		print(count)
 		#Compute the best w
 
-		w = (lmb*i+x_m.T*x_m).I * (x_m.T*y_v)
+		w = (lmb*i+x_train_m.T*x_train_m).I * (x_train_m.T*y_train_v)
 
 		#Calculate e_in and e_out
 
-		e_in_arr.append(err_fnt(w,x_m,y_v))
+		e_train_arr.append(err_fnt(w,x_train_m,y_train_v))
+		e_val_arr.append(err_fnt(w,x_val_m,y_val_v))
 		e_out_arr.append(err_fnt(w,x_out_m,y_out_v))
 
-plt.plot([i for i in range(-10,3)],e_in_arr,'bo',[i for i in range(-10,3)],e_out_arr,'ro')
+x_axis =  [i for i in range(-10,3)]
+plt.plot(x_axis,e_train_arr,'bo',x_axis,e_val_arr,'ro',x_axis,e_out_arr,'go')
 #plt.show()
 
-total = [e_in_arr,e_out_arr]
+total = [e_train_arr,e_val_arr,e_out_arr]
 print(np.matrix(total).T)
+
+d = dict(zip(e_val_arr,lmb_arr))
+lmb = d[max(e_val_arr)]
+w = (lmb*i+x_m.T*x_m).I * (x_m.T*y_v)
+print(err_fnt(w,x_m,y_v))
+print(err_fnt(w,x_out_m,y_out_v))
+
+
 
 
--- Hw4/4_19-20.py
+++ Hw4/4_19-20.py
@@ -15,6 +15,14 @@ with open('./hw4_train.dat','r') as f:
 x_m = np.matrix(x)
 y_v = np.matrix(y).T
 i = np.identity(x_m.shape[1])
+
+x_fold = []
+for i in range(5):
+		x_fold.append(x[40*i:40*(i+1)])
+y_fold = []
+for i in range(5):
+		y_fold.append(y[40*i:40*(i+1)])
+
 x_out =[]
 y_out = []
 with open('./hw4_test.dat','r') as f:
@@ -39,24 +47,37 @@ def err_fnt(w,x_m,y_v):
 		for elm in z:
 				count = count + sign(elm[0,0])
 		return (1/float(x_m.shape[0]))*count
+def return_fold(a, fold, t = 0):
+		tmp = []
+		for i in range(5):
+				if i != a:
+					tmp.append(fold[i])
+		x = np.matrix(np.concatenate(tmp))
+		if t == 0:
+				return(np.matrix(fold[a]), x)
+		else:
+				return(np.matrix(fold[a]).T,x.T)
+			
+def w_cal(lmb,x_m,y_v):
+		return (lmb*i+x_m.T*x_m).I * (x_m.T*y_v)
+
 
 lmb_arr = [10**i for i in range(-10,3)]
-e_in_arr = []
-e_out_arr = []
+err_arr = []
 for lmb in lmb_arr:
-		#Compute the best w
+		err = 0
+		for i in range(5):
+				x_val_m,x_train_m  = return_fold(i,x_fold)
+				y_val_v,y_train_v = return_fold(i,y_fold,1)
+				w=w_cal(lmb,x_train_m,y_train_v)
+				err = err + err_fnt(w,x_val_m,y_val_v)
+		err_arr.append(err/5)
 
-		w = (lmb*i+x_m.T*x_m).I * (x_m.T*y_v)
+lmb_b = dict(zip(err_arr,lmb_arr))[min(err_arr)]
+print(lmb_b)
+w=w_cal(lmb_b,x_m,y_v)
+print(err_fnt(w,x_out_m,y_out_v))
+print(err_fnt(w,x_m,y_v))
 
-		#Calculate e_in and e_out
-
-		e_in_arr.append(err_fnt(w,x_m,y_v))
-		e_out_arr.append(err_fnt(w,x_out_m,y_out_v))
-
-plt.plot([i for i in range(-10,3)],e_in_arr,'bo',[i for i in range(-10,3)],e_out_arr,'ro')
-#plt.show()
-
-total = [e_in_arr,e_out_arr]
-print(np.matrix(total).T)
 
 
